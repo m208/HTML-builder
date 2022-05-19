e8f51c54af81cc8a328eb227a9bcd901208aca3f
m208 <avcvet@yandex.ru>
2022-05-19 20:59:04 +0300

    fix: 05-merge-styles add \n to bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const fsp = fs.promises;
 const path = require('path');
+const { Transform } = require('stream');
 
 asyncJob();
 
@@ -8,13 +9,19 @@ async function asyncJob(){
   const bundle = getPath('project-dist/bundle.css');
   await fsp.writeFile(bundle, '');  
   const writeTo = fs.createWriteStream(bundle, 'utf8');
+  const addLines = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString() + '\n');
+    },
+  });
 
   const files = await getFiles(getPath('styles'));
   if(files){
     const cssFiles = files.filter(el => path.extname(el.name).match(/.css/ig));
     for (const file of cssFiles) {
       const readFrom = fs.createReadStream(getPath(`styles/${file.name}`), 'utf8');
-      readFrom.pipe(writeTo);
+      readFrom.pipe(addLines);
+      readFrom.on('end', ()=>{ addLines.pipe(writeTo); });
     }
   }
 }
