25db6419493323f447538997326a8cceb4bb8543
m208 <avcvet@yandex.ru>
2022-05-20 03:45:39 +0300

    refactor: 06-build-page rework

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,124 +7,129 @@ const { Transform } = require('stream');
 doAsyncJob();
 
 
-async function doAsyncJob(){
+async function doAsyncJob() {
+  const dist = 'project-dist';
+  const dirExist = await isDirExist(getPath(dist));
 
-  const dir = getPath('project-dist');
-  const dirExist = await isDirExist(dir); 
-  let errors = false;
-    
-  if(dirExist) { 
-    try { await fsp.rmdir(dir, {recursive:true}); }
-    catch (err) { console.log(err + '\nCan\'t remove dir!');  errors = true;}
-  }
-  if(errors) return; 
-  
-  await fsp.mkdir(dir); 
-
-  await copyCss(getPath('styles'), getPath('project-dist/style.css'));
-
-  const components = await getFiles(getPath('components'));
-  const templates = new Map();
-
-  for (const comp of components){
-    const shortCut = `{{${comp.name.slice(0,-5)}}}`;
-    try{
-      const content = await fsp.readFile(getPath(`components/${comp.name}`), 'utf-8');
-      templates.set(shortCut, content);
-    }
-    catch (err) { console.log(err); }
+  if (dirExist) {
+    try { await fsp.rmdir(getPath(dist), { recursive: true }); }
+    catch (err) { console.log(err + '\nCan\'t remove dir!'); return null; }
   }
 
-  const readFile = fs.createReadStream(getPath('template.html'), 'utf8');
-  const writeFile = fs.createWriteStream(getPath('project-dist/index.html'), 'utf8');
+  await fsp.mkdir(getPath(dist));
+  await generateHTML('template.html', dist + '/index.html');
+  await generateCSS('styles', dist + '/style.css');
+  await recursiveCopyDir('assets', dist + '/assets/');
+}
 
-  const insertion = new Transform({
-    transform(chunk, encoding, callback) {
+async function generateHTML(from, to) {
+
+  const tags = await getTemplateTags(from);
+
+  const readFile = fs.createReadStream(getPath(from), 'utf8');
+  const writeFile = fs.createWriteStream(getPath(to), 'utf8');
+
+  const tagInsert = new Transform({
+    async transform(chunk, encoding, callback) {
       let data = chunk.toString();
 
-      templates.forEach((content, shortCut)=>{
-        if (data.includes(shortCut)){
-          data = data.replace(shortCut, content);
+      for (const tag of tags) {
+        if (data.includes(tag)) {
+          const tagName = tag.replace(/{/g, '').replace(/}/g, '').trim();
+          const tagPath = (getPath('components/' + tagName + '.html'));
+          const tagContent = await fsp.readFile(tagPath, 'utf-8');
+          data = data.replace(tag, tagContent);
         }
-      });
-
+      }
       callback(null, data);
     },
   });
 
-  readFile.pipe(insertion).pipe(writeFile);
-
-  copyDirectory();
+  readFile.pipe(tagInsert).pipe(writeFile);
 }
 
-async function copyCss(from, to){
-  const writeTo = fs.createWriteStream(to, 'utf8');
-  
-  const files = await getFiles(from);
-  if(files){
+async function getTemplateTags(from) {
+  return new Promise(function (resolve) {
+
+    const readFile = fs.createReadStream(getPath(from), 'utf8');
+    const tags = [];
+
+    readFile.on('data', (data) => {
+      const mdata = data.split('\n')
+        .filter(el => el.includes('{{') && el.includes('}}'));
+
+      mdata.forEach(el => { tags.push(el.trim()); });
+
+      resolve(tags);
+    });
+  });
+}
+
+async function generateCSS(from, to) {
+  const writeTo = fs.createWriteStream(getPath(to), 'utf8');
+
+  const addLines = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString() + '\n\n');
+    },
+  });
+
+  const files = await getFiles(getPath(from));
+  if (files) {
     const cssFiles = files.filter(el => path.extname(el.name).match(/.css/ig));
+
     for (const file of cssFiles) {
-      const filePath = path.join(from, file.name);
+      const filePath = path.join(getPath(from), file.name);
       const readFrom = fs.createReadStream(filePath, 'utf8');
-      readFrom.pipe(writeTo);
+      readFrom.pipe(addLines);
+      readFrom.on('end', () => { addLines.pipe(writeTo); });
     }
   }
 }
-async function getFiles(dirPath){
-  try {
-    const files = await fsp.readdir(dirPath, {withFileTypes: true});
-    return files.filter(el=>!el.isDirectory());
-  } 
-  catch (err) { return null; }
-}
-  
-function getPath(dir){
-  return path.join(__dirname, dir);
-}
-  
-async function isDirExist(dir){
-  try { await fsp.access(dir); } 
-  catch (err) { return false; }
-  return true;
-}
- 
-async function copyDirectory(){
-  const copy = getPath('project-dist/assets');
-  const dirExist = await isDirExist(copy); 
-  let errors = false;
-    
-  if(dirExist) { 
-    try { await fsp.rmdir(copy, {recursive:true}); }
-    catch (err) { console.log(err + '\nCan\'t remove dir!');  errors = true;}
-  }
-  if(errors) return;
-  await fsp.mkdir(copy); 
 
-  await recDir(path.join(__dirname, 'assets'), 'assets/');
-}
+async function recursiveCopyDir(dir, subPath) {
 
-
-async function recDir(dirPath, subPath){
-
-  const makeDir = path.join(__dirname, 'project-dist/' + subPath); 
-  const dirExist = await isDirExist(makeDir); 
-  if(!dirExist) await fsp.mkdir(makeDir); 
+  const makeDir = path.join(__dirname, subPath);
+  const dirExist = await isDirExist(makeDir);
+  if (!dirExist) await fsp.mkdir(makeDir);
 
   try {
-    const items = await fsp.readdir(dirPath, {withFileTypes: true});
+    const items = await fsp.readdir(getPath(dir), { withFileTypes: true });
 
-    for (const item of items){
+    for (const item of items) {
       if (item.isFile()) {
-        const copy = path.join(__dirname, 'project-dist/' + subPath + item.name); 
-        const current = path.join(dirPath, item.name);  
+        const copy = path.join(__dirname, subPath + item.name);
+        const current = path.join(getPath(dir), item.name);
         await fsp.copyFile(current, copy);
       }
       else {
-        const next = path.join(dirPath, item.name); 
-        await recDir(next, subPath + item.name + '/');
+        const next = path.join(dir, item.name);
+        await recursiveCopyDir(next, subPath + item.name + '/');
       }
     }
 
-  } 
+  }
   catch (err) { return null; }
 }
+
+async function getFiles(dirPath) {
+  try {
+    const files = await fsp.readdir(dirPath, { withFileTypes: true });
+    return files.filter(el => !el.isDirectory());
+  }
+  catch (err) { return null; }
+}
+
+function getPath(dir) {
+  return path.join(__dirname, dir);
+}
+
+async function isDirExist(dir) {
+  try { await fsp.access(dir); }
+  catch (err) { return false; }
+  return true;
+}
+
+
+
+
